63671039dbf30ec2fc7a187ddd433dc3ecd93f2b
spalutin <spalutin@starsgroup.com>
2021-12-19 23:22:46 +0000

    day 18 1

--- src/day_18.rs
+++ src/day_18.rs
@@ -1,35 +1,35 @@
 use std::fmt;
 
-enum Item {
+#[derive(Clone)]
+enum SnailFish {
     N(u64),
-    S(SnailFish),
+    S(Box<Self>, Box<Self>),
 }
-
-struct SnailFish(Box<Item>, Box<Item>);
 
 
 impl fmt::Display for SnailFish {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let a = match &*self.0 {
-            Item::N(n) => format!("{}", n),
-            Item::S(s) => format!("{}", s),
-        };
-        let z = match &*self.1 {
-            Item::N(n) => format!("{}", n),
-            Item::S(s) => format!("{}", s),
-        };
-        write!(f, "[{},{}]", a, z)
+        match self {
+            Self::N(n) => write!(f, "{}", n),
+            Self::S(a, b) => {
+                write!(f, "[{},{}]", *a, *b)
+            }
+        }
     }
 }
 
 impl SnailFish {
     pub fn from(line: &str) -> Self {
-        let parse_item = |x: &str| Box::new(if &x[0..1] == "[" {
-            Item::S(Self::from(x))
-        } else {
-            Item::N(x.parse().unwrap())
-        });
+        if &line[0..1] != "[" {
+            return Self::N(line.parse().unwrap());
+        }
 
+        // let parse_item = |x: &str| Box::new(if &x[0..1] == "[" {
+        //     Item::S(Self::from(x))
+        // } else {
+        //     Item::N(x.parse().unwrap())
+        // });
+        //
         let top = line.len() - 1;
         let mut iter = line.chars();
         let mut ix = 0;
@@ -42,17 +42,20 @@ impl SnailFish {
                 ',' => if opened == 1 { break; },
                 _ => {}
             }
-            // println!("ix: {}, char: '{}', opened: {}", ix, c, opened);
             ix += 1;
         }
-
-        Self(parse_item(&line[1..ix]), parse_item(&line[ix + 1..top]))
+        Self::S(
+            Box::new(SnailFish::from(&line[1..ix])),
+            Box::new(SnailFish::from(&line[ix + 1..top])),
+        )
     }
 
-    pub fn add(a: Self, b: Self) -> Self {
-        Self(Box::new(Item::S(a)), Box::new(Item::S(b)))
+    pub fn add(self, other: Self) -> Self {
+        Self::S(
+            Box::new(self),
+            Box::new(other),
+        )
     }
-
 }
 
 
@@ -75,7 +78,7 @@ mod tests {
 
         let fish = "[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]";
         assert_eq!(format!("{}", SnailFish::from(fish)), fish);
-        assert_eq!(format!("{}", SnailFish::add(SnailFish::from("[1,2]"), SnailFish::from("[[3,4],5]"))), "[[1,2],[[3,4],5]]");
+        assert_eq!(format!("{}", SnailFish::from("[1,2]").add(SnailFish::from("[[3,4],5]"))), "[[1,2],[[3,4],5]]");
         assert_eq!(solve_18_1(&test_data), 4140);
         assert_eq!(solve_18_2(&test_data), 0);
     }
